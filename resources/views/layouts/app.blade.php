79206aa95940f7916c045c19ca76e78a46a5d1de
sharnaaysha23@gmail.com <sharnaaysha23@gmail.com>
2019-05-14 15:56:35 +0900

    layout using html,css,bootstrap,javascript

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="UTF-8">
+        <title>M&F Tecnica</title>
+        <link rel="stylesheet" href="./css/bootstrap.css" />
+        <link rel="stylesheet" href="./css/style.css" />
+    </head>
+    <body>
+        @include('inc.jumbotron')
+    </body>
+
+</html>
